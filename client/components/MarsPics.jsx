4c1dc841d1c476efe8f5f0760fc48fa7ed8116d2
Mathias Bast <mathiasbast123@gmail.com>
2020-04-29 18:16:52 +1200

    moves form to app.jsx

--- client/components/MarsPics.jsx
+++ client/components/MarsPics.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import request from 'superagent'
 
-const apiUrl = '/api/v1/mars'
+
 
 const imgStyle = {
   width: '100%',
@@ -10,105 +9,18 @@ const imgStyle = {
   marginBottom: '20px'
 }
 
-let viewData = {
-  sol: 1000,
-  camera: 'FHAZ'
-}
+
 
 class MarsPics extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      sol: 1000,
-      camera: 'FHAZ',
-      load: false,
-      pics: '',
-      state: 'loading...'
-    }
-  }
-
-  loader () {
-    var { sol, camera } = viewData
-    request.get(apiUrl + '/' + sol + '/' + camera)
-      .then(res => {
-        this.setState({
-          pics: res.body.pics
-        })
-      })
-      .then(() => {
-        if (this.state.pics.length === 0) {
-          this.setState({
-            state: 'No images found',
-            load: false
-          })
-        } else {
-          this.setState({
-            state: 'loading...',
-            load: true
-          })
-        }
-      })
-  }
-
-  componentDidMount () {
-    this.loader()
-  }
-
-  handleCameraChange = event => {
-    this.setState({
-      camera: event.target.value
-    })
-  }
-
-  handleSolChange = event => {
-    this.setState({
-      sol: event.target.value
-    })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-    const { sol, camera } = this.state
-    viewData = {
-      sol: sol,
-      camera: camera
-    }
-    this.setState({
-      load: false
-    })
-    this.loader()
-  }
 
   render () {
     return (
       <>
         <div className='div-class'>
           <h1>Mars Rover Pix</h1>
-          <form>
-            <label>
-            Pick a camera
-              <select name='camera' value={this.state.camera} onChange={this.handleCameraChange}>
-                <option value='FHAZ'>Front Hazard Avoidance Camera</option>
-                <option value="RHAZ">Rear Hazard Avoidance Camera</option>
-                <option value="MAST">Mast Camera</option>
-                <option value="CHEMCAM">Chemistry and Camera Complex</option>
-                <option value="MAHLI">Mars Hand Lens Imager</option>
-                <option value="MARDI">Mars Descent Imager</option>
-                <option value="NAVCAM">Navigation Camera</option>
-              </select>
-            </label>
-            <br></br>
-            <br></br>
-            <label>Sol </label><input min={0} name='sol' type='number'
-              value={this.state.sol}
-              onChange={this.handleSolChange}
-              placeholder={0}
-            />
-            <input type="submit" value="Submit" onClick={this.handleSubmit} />
-          </form>
+          
 
-          {this.state.load
+          {this.props.load
             ? <div>
               <p>Max Sol is: {this.state.pics[0].maxSol}</p>
               {this.state.pics.map(pic => {
--- server/routes/MarsPic.js
+++ server/routes/MarsPic.js
@@ -7,28 +7,55 @@ const apiKey = '60Db4uxfwdIj8Jjsp5gM3q1cUy9PVFmTfYH2uRmD'
 // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=5&camera=FHAZ&api_key=60Db4uxfwdIj8Jjsp5gM3q1cUy9PVFmTfYH2uRmD
 router.get('/:sol/:camera', (req, res) => {
   const { sol, camera } = req.params
-  request.get(api)
-    .query({ sol: sol })
-    .query({ camera: camera })
-    .query({ api_key: apiKey })
-    .then(apiRes => {
-      let viewData = {
-        pics: []
-      }
-      apiRes.body.photos.map((photo, i) => {
-        const { id, sol, img_src, earth_date, rover } = photo
-        const data = {
-          id: id,
-          sol: sol,
-          imgSrc: img_src,
-          earthDate: earth_date,
-          roverName: rover.name,
-          maxSol: rover.max_sol
+  if(camera !== 'ALL'){
+    request.get(api)
+      .query({ sol: sol })
+      .query({ camera: camera })
+      .query({ api_key: apiKey })
+      .then(apiRes => {
+        let viewData = {
+          pics: []
         }
-        viewData.pics.push(data)
+        apiRes.body.photos.map((photo, i) => {
+          const { id, sol, img_src, earth_date, rover } = photo
+          const data = {
+            id: id,
+            sol: sol,
+            imgSrc: img_src,
+            earthDate: earth_date,
+            roverName: rover.name,
+            maxSol: rover.max_sol
+          }
+          viewData.pics.push(data)
+        })
+        res.json(viewData)
       })
-      res.json(viewData)
-    })
+  }
+  if(camera === 'ALL') {
+    request.get(api)
+      .query({ sol: sol })
+      .query({ api_key: apiKey })
+      .then(apiRes => {
+        let viewData = {
+          pics: []
+        }
+        apiRes.body.photos.map((photo, i) => {
+          const { id, sol, img_src, earth_date, rover } = photo
+          const data = {
+            id: id,
+            sol: sol,
+            imgSrc: img_src,
+            earthDate: earth_date,
+            roverName: rover.name,
+            maxSol: rover.max_sol
+          }
+          viewData.pics.push(data)
+        })
+        res.json(viewData)
+      })
+  }
 })
+
+// 
 
 module.exports = router
